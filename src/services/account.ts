c282e8f9514fb86386fb816fa045c9c49cb817e0
alexsuperclimb <alex.sumaila@climbcred.it>
2022-06-27 12:29:16 +0100

    Change bcrypt lib to bcryptjs

--- src/services/account.ts
+++ src/services/account.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import Accounts from '../models/accounts';
 import * as config from '../config/dev.json';
